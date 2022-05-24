404080b7310350e3873c4da1177385c086100c4a
Arnold Gashi <arnold.gashi@icebaum.com>
2022-05-24 15:38:46 +0200

    ss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Home } from './components/home/home';
 import SignIn from './components/login/signin';
@@ -7,33 +6,23 @@ import SignUp from './components/login/signup';
 import { StoryItem } from './components/story/storyitem';
 import { Container } from '@mui/system';
 import { Header } from './components/header/header';
-=======
-import { useEffect, useState } from "react";
-import "./App.css";
-import { Home } from "./components/home/home";
-import SignIn from "./components/login/signin";
-import SignUp from "./components/login/signup";
-import { StoryItem } from "./components/story/storyitem";
-import { Container } from "@mui/system";
-import { Header } from "./components/header/header";
->>>>>>> 0accec86ce36d1d01ac39e2808c445d6ca82cdb3
 const App = () => {
-  const [IsLoggedIn, setIsLoggedIn] = useState(false);
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      setIsLoggedIn(true);
-    } else {
-    }
-  }, []);
+	const [IsLoggedIn, setIsLoggedIn] = useState(false);
+	useEffect(() => {
+		const token = localStorage.getItem('token');
+		if (token) {
+			setIsLoggedIn(true);
+		} else {
+		}
+	}, []);
 
-  return (
-    <Container maxWidth='lg'>
-      <Header IsLoggedIn={IsLoggedIn} />
-      {/* {token? <SignIn />: <SignUp />} */}
-      <Home />
-    </Container>
-  );
+	return (
+		<Container maxWidth='lg'>
+			<Header IsLoggedIn={IsLoggedIn} />
+			{/* {token? <SignIn />: <SignUp />} */}
+			<Home />
+		</Container>
+	);
 };
 
 export default App;
